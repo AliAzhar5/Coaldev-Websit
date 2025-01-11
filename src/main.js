33d773b3631cf46ccfaff14733f8c00a18085a77
AliAzhar5 <aalliiazhar@gmail.com>
2025-01-11 15:58:16 +0500

    Applied Lazy Loading

--- src/main.js
+++ src/main.js
@@ -2,14 +2,7 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
-import Home from "./views/Home.vue";
-import Services from "./views/Services.vue";
-import Industries from "./views/Industries.vue";
-import About from "./views/About.vue";
 import Header from "./components/Header.vue";
-import IndustryDetails from "./views/IndustryDetails.vue";
-import Contact from "./views/contact.vue";
-import FAQ from "./views/FAQ.vue";
 import ChatButton from "./components/ChatButton.vue";
 import VueGtag from "vue-gtag";
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -20,15 +13,23 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 // Add Font Awesome icons to the library
 library.add(fas, fab);
 
+// Lazy load route components
 const routes = [
   { path: "/", redirect: "/home" },
-  { path: "/home", component: Home },
-  { path: "/services", component: Services },
-  { path: "/industries", component: Industries },
-  { path: "/industry/:name", component: IndustryDetails },
-  { path: "/about", component: About },
-  { path: "/contact", name: "Contact", component: Contact },
-  { path: "/faq", name: "FAQ", component: FAQ },
+  { path: "/home", component: () => import("./views/Home.vue") },
+  { path: "/services", component: () => import("./views/Services.vue") },
+  { path: "/industries", component: () => import("./views/Industries.vue") },
+  {
+    path: "/industry/:name",
+    component: () => import("./views/IndustryDetails.vue"),
+  },
+  { path: "/about", component: () => import("./views/About.vue") },
+  {
+    path: "/contact",
+    name: "Contact",
+    component: () => import("./views/contact.vue"),
+  },
+  { path: "/faq", name: "FAQ", component: () => import("./views/FAQ.vue") },
 ];
 
 const router = createRouter({
